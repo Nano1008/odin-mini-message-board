904ebdb9a7697a1e2269e747a0bc2bf033f035f3
Sinan <nanossn1008@gmail.com>
2025-05-23 22:32:33 +0100

    Implement home page

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const router = Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+// GET home page
+router.get("/", (req, res) => {
+  res.render("index", { title: "Mini Message Board", messages });
+});
+
+module.exports = router;
